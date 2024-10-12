2d8efdc4c5c529502dd55afe2423a8c0305203e9
n25a <nimashadix@gmail.com>
2024-10-12 23:57:06 +0330

    🥤 add log package

---
+++ internal/log/logger.go
@@ -0,0 +1,12 @@
+package log
+
+import "go.uber.org/zap"
+
+var (
+	// Logger is the default logger of the application
+	Logger *zap.Logger
+)
+
+func init() {
+	Logger, _ = zap.NewProduction(zap.WithCaller(false))
+}
